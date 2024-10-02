05b0b42775e12b35474e5467fb1a0e0f0d5cc529
quachvankhanh <khanhqvj@gmail.com>
2024-10-02 17:21:01 +0700

    update founder and animation buttom

--- components/HeroOd.js
+++ components/HeroOd.js
@@ -1,73 +1,76 @@
-import React from 'react';
-var $ = require('jquery');
-import Image from 'next/image';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
+import React from "react";
+var $ = require("jquery");
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
-  const variants = {
-    hidden: { opacity: 0, y: -100 }, 
-    visible: { opacity: 1, y: 0 },
-    transition: { duration: 2 },  
-  };
-  const variants1 = {
-    hidden: { opacity: 0, y: 100 }, 
-    visible: { opacity: 1, y: 0 },
-    transition: { duration: 2 },  
-  };
-  const variants2 = {
-    hidden: { opacity: 0, x: 100 }, 
-    visible: { opacity: 1, x: 0 },
-    transition: { duration: 2 },  
-  };
-  const variants3 = {
-    hidden: { opacity: 0, x: -100 }, 
-    visible: { opacity: 1, x: 0 },
-    transition: { duration: 2 },  
-  };
-export default function HeroAbout({ title, content}) {
+const variants = {
+  hidden: { opacity: 0, y: -100 },
+  visible: { opacity: 1, y: 0 },
+  transition: { duration: 2 },
+};
+const variants1 = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0 },
+  transition: { duration: 2 },
+};
+const variants2 = {
+  hidden: { opacity: 0, x: 100 },
+  visible: { opacity: 1, x: 0 },
+  transition: { duration: 2 },
+};
+const variants3 = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0 },
+  transition: { duration: 2 },
+};
+export default function HeroAbout({ title, content }) {
   return (
-    <div className='relative flex flex-col gap-10 lg:gap-20 max-sm:pb-0 overflow-hidden lg:h-[45rem] md:h-[36rem] max-2xl:h-[45rem] max-sm:h-60'>
-  <div className="relative text-center ">
-    <div className="absolute inset-0 z-10 flex flex-col items-center justify-center">
-      <motion.span 
-        initial="hidden"
-        animate="visible"
-        variants={variants}
-        transition={variants.transition}
-        className="text-sm lg:text-5xl font-semibold text-white uppercase custom-shadow" >
-        {title}
-      </motion.span>
-      <div className='flex flex-row items-center whitespace-nowrap'>
-        <Link href={'/'}>
-          <motion.p 
+    <div className="relative flex flex-col gap-10 lg:gap-20 max-sm:pb-0 overflow-hidden lg:h-[20rem] md:h-[36rem] max-2xl:h-[20rem] max-sm:h-60">
+      <div className="relative text-center">
+        <div className="absolute w-full h-full z-10 flex flex-col items-center justify-center">
+          <motion.span
             initial="hidden"
             animate="visible"
-            variants={variants3}
-            transition={variants3.transition}
-            className="text-white font-medium text-3xl max-sm:text-sm uppercase py-3 drop-shadow-lg custom-shadow">
-            Home /
-          </motion.p>
-        </Link>
-        <motion.span
-          initial="hidden"
-          animate="visible"
-          variants={variants2}
-          transition={variants2.transition}
-          className="lg:text-base text-sm text-blue-700 font-medium max-sm:text-xs 2xl:max-w-[50%] 2xl:text-2xl underline pl-2 custom-shadow">
-           {content}
-        </motion.span>
+            variants={variants}
+            transition={variants.transition}
+            className="lg:text-5xl  font-semibold text-white uppercase custom-shadow 2xl:text-5xl md:text-3xl max-sm:text-xl"
+          >
+            {title}
+          </motion.span>
+          <div className="flex flex-row items-center justify-center whitespace-nowrap">
+            <Link href={"/"}>
+              <motion.p
+                initial="hidden"
+                animate="visible"
+                variants={variants3}
+                transition={variants3.transition}
+                className="text-white font-medium text-3xl max-sm:text-sm uppercase py-3 drop-shadow-lg custom-shadow 2xl:text-2xl lg:text-base xl:text-xl md:text-lg"
+              >
+                Home /
+              </motion.p>
+            </Link>
+            <motion.span
+              initial="hidden"
+              animate="visible"
+              variants={variants2}
+              transition={variants2.transition}
+              className="lg:text-base text-sm text-blue-700 font-medium max-sm:text-xs 2xl:max-w-[50%] 2xl:text-2xl underline pl-2 custom-shadow"
+            >
+              {content}
+            </motion.span>
+          </div>
+        </div>
+        <div className="relative item  overflow-hidden">
+          <Image
+            src="/assets/img/ai2.jpg"
+            alt="ai hero image"
+            className="brightness-75 w-full h-auto object-cover lg:h-[20rem] md:h-[36rem] max-2xl:h-[20rem] max-sm:h-60 "
+            width={1280}
+            height={700}
+          />
+        </div>
       </div>
     </div>
-    <div className="relative item h-[45rem] overflow-hidden">
-      <Image
-        src="/assets/img/ai2.jpg"
-        alt="ai hero image"
-        className="w-full h-auto object-cover max-h-[45rem] max-sm:h-[15rem] max-md:h-[22rem] brightness-95" 
-        width={1280}
-        height={700}
-      />
-    </div>
-  </div>
-</div>
   );
 }
--- components/SliderProduct.js
+++ components/SliderProduct.js
@@ -1,87 +1,114 @@
-import React from 'react'
-import Link from 'next/link';
-var $ = require('jquery');
+import React from "react";
+import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-if (typeof window !== 'undefined') {
-    window.$ = window.jQuery = require('jquery');
-  }
-  import 'owl.carousel/dist/assets/owl.carousel.css';
-  import 'owl.carousel/dist/assets/owl.theme.default.css';
-  import dynamic from 'next/dynamic';
-  
-  const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
-    ssr: false,
-  });
 function SliderProduct() {
-    const products =[
-        { src: '/assets/img/product.jpg', alt: '1', name: 'Kênh Động vật', topic:'Chủ đề', sub: '1234', views:'123', link:'https://www.youtube.com/'},
-        { src: '/assets/img/product.jpg', alt: '2', name: 'Kênh Động vật' , topic:'Chủ đề', sub: '1234', views:'123', link:'https://www.youtube.com/'},
-        { src: '/assets/img/product.jpg', alt: '3', name: 'Kênh Động vật' , topic:'Chủ đề', sub: '1234', views:'123', link:'https://www.youtube.com/'}  
-      ];
+  const products = [
+    {
+      src: "/assets/img/product.jpg",
+      alt: "1",
+      name: "Kênh Động vật",
+      topic: "Chủ đề",
+      sub: "1234",
+      views: "123",
+      link: "https://www.youtube.com/",
+    },
+    {
+      src: "/assets/img/product.jpg",
+      alt: "2",
+      name: "Kênh Động vật",
+      topic: "Chủ đề",
+      sub: "1234",
+      views: "123",
+      link: "https://www.youtube.com/",
+    },
+    {
+      src: "/assets/img/product.jpg",
+      alt: "3",
+      name: "Kênh Động vật",
+      topic: "Chủ đề",
+      sub: "1234",
+      views: "123",
+      link: "https://www.youtube.com/",
+    },
+  ];
   return (
-    <div className='item w-full flex flex-col relative items-center justify-center p-4 pb-20'>
-  <div className='flex flex-col relative items-center justify-center text-center'>
-    <span className='font-black text-sm lg:text-4xl leading-[-130%]'>Products</span>
-    <div className="h-[3px] w-[81px] md:w-[200px] lg:w-[250px] xl:w-[140px] mt-3 m-auto md:m-4 md:mt-1 rounded-[20px] bg-[#144189] max-sm:mb-2 max-sm:w-[4rem]"></div>
-  </div>
+    <div className="item w-full flex flex-col relative items-center justify-center p-4 pb-20">
+      <div className="flex flex-col relative items-center justify-center text-center">
+        <span className="font-black text-sm lg:text-4xl leading-[-130%]">
+          Products
+        </span>
+        <div className="h-[3px] w-[81px] md:w-[200px] lg:w-[250px] xl:w-[140px] mt-3 m-auto md:m-4 md:mt-1 rounded-[20px] bg-[#144189] max-sm:mb-2 max-sm:w-[4rem]"></div>
+      </div>
 
-  <div className="w-8/12">
-  <OwlCarousel
-    items={1}
-    dots={false}
-    loop={false}
-    margin={10}
-    responsive={{
-      1: { items: 1, dots: true },
-      900: { items: 1 },
-      1025: { items: 3 },
-    }}
-  >
-    {products.map((product, index) => (
-      <div className="relative h-[350px] lg:h-[400px] xl:h-[500px] flex flex-col group card" key={index}>
-        <div className="absolute inset-0 overflow-hidden">
-          <img
-            src={product.src}
-            alt={product.alt}
-            className="w-full h-full object-cover rounded-xl transition-all duration-300 ease-in-out"
-            width={1280}
-            height={700}
-            priority
-            layout="intrinsic"
-          />
-        </div>
-        <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white p-4 rounded-xl opacity-100 transition-opacity duration-300 ease-in-out group-hover:opacity-0 z-10">
-            <h1 className="font-bold text-xl sm:text-lg md:text-xl lg:text-2xl mt-auto">{product.name}</h1>
-        </div>
+      <div className="w-8/12">
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={true}
+          breakpoints={{
+            480: { slidesPerView: 1, spaceBetween: 20 },
+            1024: { slidesPerView: 3, spaceBetween: 30 },
+          }}
+        >
+          {products.map((product, index) => (
+            <SwiperSlide key={index}>
+              <div className="relative h-[350px] lg:h-[400px] xl:h-[500px] flex flex-col group card">
+                <div className="absolute inset-0 overflow-hidden">
+                  <img
+                    src={product.src}
+                    alt={product.alt}
+                    className="w-full h-full object-cover rounded-xl transition-all duration-300 ease-in-out"
+                    width={1280}
+                    height={700}
+                    priority="true"
+                  />
+                </div>
+                <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white p-4 rounded-xl opacity-100 transition-opacity duration-300 ease-in-out group-hover:opacity-0 z-10">
+                  <h1 className="font-bold text-xl sm:text-lg md:text-xl lg:text-2xl mt-auto">
+                    {product.name}
+                  </h1>
+                </div>
 
-        <div className="absolute inset-0 flex flex-col text-white p-4 rounded-xl bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 z-20">
-             <h1 className="font-bold text-lg sm:text-base md:text-lg lg:text-xl mb-2">{product.name}</h1>
-            <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">Topic: {product.topic}</p>
-             <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">{product.views} views</p>
-            <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">{product.sub} subscribe</p>
-        <div className="mt-auto flex items-center justify-center w-full">
-    <Link href={product.link} className="w-10/12">
-    <div className='flex flex-row p-2 bg-[#3f71fe] text-center items-center justify-center rounded-lg'>
-      <h1 className='text-sm font-medium w-8/12'>Go to view</h1>
-      <div className='w-4/12 ml-auto'>
-        <img
-          className="w-4 h-4 text-white"
-          alt="arrow-right"
-          src="assets/svg/arrow-right.svg"
-        />
+                <div className="absolute inset-0 flex flex-col text-white p-4 rounded-xl bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 z-20">
+                  <h1 className="font-bold text-lg sm:text-base md:text-lg lg:text-xl mb-2">
+                    {product.name}
+                  </h1>
+                  <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">
+                    Topic: {product.topic}
+                  </p>
+                  <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">
+                    {product.views} views
+                  </p>
+                  <p className="text-sm sm:text-xs md:text-sm lg:text-base mb-2">
+                    {product.sub} subscribe
+                  </p>
+                  <div className="mt-auto flex items-center justify-center w-full">
+                    <Link href={product.link} className="w-10/12">
+                      <div className="flex flex-row p-2 bg-[#3f71fe] text-center items-center justify-center rounded-lg">
+                        <h1 className="text-sm font-medium w-8/12">
+                          Go to view
+                        </h1>
+                        <div className="w-4/12 ml-auto">
+                          <img
+                            className="w-4 h-4 text-white"
+                            alt="arrow-right"
+                            src="assets/svg/arrow-right.svg"
+                          />
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
-  </Link>
-</div>
-</div>
-      </div>
-    ))}
-  </OwlCarousel>
-</div>
-</div>
-
-
-  )
+  );
 }
-
-export default SliderProduct
+export default SliderProduct;
